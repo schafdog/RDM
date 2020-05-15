bfa2f5c6650105bf218b2c017a35a6c5d1e01b17
Robert Read <rread@bitsoflight.com>
2020-05-15 11:12:15 -0400

    Use getopt_long() to standardize command processing
    Also prevents segfaults when missing parameters are passed.
    Switching to getops forced changng nonstandard short comamnds
    "-ld" and "-lm" to "-l" and "-m", respectively.

--- main.mm
+++ main.mm
@@ -7,33 +7,31 @@
 #import "cmdline.h"
 
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
 	int ret = -1;
 	if(argc > 1)
 	{
 		ret = cmdline_main(argc, argv);
-	}
 	
-//	if(ret == -1)
-	{
-		fprintf(stderr, "Commandline options\n"
+		if(ret == -1)
+		{
+			fprintf(stderr, "Commandline options\n"
 						"  --width    (-w)  Width\n"
 						"  --height   (-h)  Height\n"
 						"  --scale    (-s)  Scale (2.0 = Retina, default=current)\n"
 						"  --bits     (-b)  Color depth (default=current)\n"
 						"  --display  (-d)  Select display # (default=main)\n"
-						"  --displays (-ld) List available displays\n"
-						"  --modes    (-lm) List available modes\n"
-						"\nCurrently running GUI.  Use ^C or close from menu\n");
-		
-		NSApplication* app = [NSApplication sharedApplication];
-
-		[app setDelegate: [SRApplicationDelegate new]];
-
-		//NSApplication* app = [SRApplication sharedApplication];
-		[app performSelectorOnMainThread: @selector(run) withObject: nil waitUntilDone: YES];
+						"  --displays (-l) List available displays\n"
+				                "  --modes    (-m) List available modes\n");
+		}
+		exit(ret);
 	}
-	
-	
+
+	fprintf(stdout, "Currently running GUI.  Use ^C or close from menu\n");
+
+	NSApplication* app = [NSApplication sharedApplication];
+	[app setDelegate: [SRApplicationDelegate new]];
+	//NSApplication* app = [SRApplication sharedApplication];
+	[app performSelectorOnMainThread: @selector(run) withObject: nil waitUntilDone: YES];
 }
